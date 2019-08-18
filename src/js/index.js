2ba182cbe62e4059dfb33fb1e1b0fddf1b9782a6
Scott Vandehey <fake-scott@spaceninja.com>
2019-08-18 09:34:28 -0700

    add lodash modules

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,14 @@
 import vocabulary from '../data/vocabulary.json';
 
+import sample from 'lodash.sample';
+import random from 'lodash.random';
+
+// eslint-disable-next-line no-console
+console.log('RANDOM', random(0, 5));
+
+// eslint-disable-next-line no-console
+console.log('SAMPLE', sample([1, 2, 3, 4]));
+
 // eslint-disable-next-line no-console
 console.log('Hello Project.');
 
