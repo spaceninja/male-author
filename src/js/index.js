ddac8fa4e9ed4f37ce6a701a5f1ae07a89890aed
Scott Vandehey <fake-scott@spaceninja.com>
2019-08-18 09:21:49 -0700

    disable tests for now

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,10 @@
+import vocabulary from '../data/vocabulary.json';
+
 // eslint-disable-next-line no-console
 console.log('Hello Project.');
 
 // eslint-disable-next-line no-console
 console.log(process.env.NODE_ENV);
+
+// eslint-disable-next-line no-console
+console.log(vocabulary);
