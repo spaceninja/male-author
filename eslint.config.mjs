f41093e99bd5c77eb6d5c50887deb792f02d9c5f
Scott Vandehey <fake-scott@spaceninja.com>
2025-02-20 15:18:20 -0800

    use flat config file

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  eslintConfigPrettier,
+  {
+    ignores: ['dist/**/*', '**/*.html'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.es2023,
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      // override/add rules settings here, such as:
+      // 'vue/no-unused-vars': 'error'
+    },
+  },
+];
